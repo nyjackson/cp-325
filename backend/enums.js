5d157f1a1aa4851ca6ee1e7dfce6d9e5716f8636
Nyala Jackson <nymjackson@gmail.com>
2025-11-18 10:38:50 -0500

    Adding schema for appointment, article models and added enum.js file for enums.

---
+++ backend/enums.js
@@ -0,0 +1,17 @@
+// completed, scheduled, canceled, rescheduled, confirmed, unconfirmed, missed (enum?)
+const APPT_STATUS = {
+    COMPLETE: "completed",
+    SCHEDULED: "scheduled",
+    CANCELED: "canceled",
+    RESCHEDULED: "rescheduled",
+    CONFIRMED: "confirmed",
+    UNCONFIRMED: "unconfirmed",
+    MISSED: "missed"
+}
+
+const APPT_TAX_TYPE = {
+    IND: "individal",
+    BUS: "business"
+}
+
+export default {APPT_STATUS, APPT_TAX_TYPE}
--- backend/models/appointment.js
+++ backend/models/appointment.js
@@ -2,16 +2,18 @@
  * _id (auto), name, 
  *
  */
-
+import enums from "../enums";
 import mongoose from "mongoose";
+
 const appointmentSchema = mongoose.Schema({
   client_first_name: {type:String, required: true},
   client_last_name: {type: String, required :true},
+  tax_type: {type:enums.APPT_TAX_TYPE, required:false}, // enum determines how long an appointment will be. IND: 1 hr, BUS: 2+ hrs
   date_scheduled: {type: Date, required: true},
   time_scheduled: {type: Date, required:true},
-  status: {type:String, required:true}, // completed, scheduled, canceled, rescheduled (enum?)
+  status: {type: enums.APPT_STATUS, required:true}, // completed, scheduled, canceled, rescheduled, confirmed, unconfirmed, missed (enum?)
+  notes: {type:String, required:false},
   special_requests:{type:String, required:false},
-  
 });
 
 const Appointment = mongoose.model('appointment', appointmentSchema)
