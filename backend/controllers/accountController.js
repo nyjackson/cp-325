2db7e3d15386d399bd4f647824d7a3856ac4a277
Nyala Jackson <nymjackson@gmail.com>
2025-11-20 13:38:07 -0500

    authController genHash working and stored to the db.

--- backend/controllers/accountController.js
+++ backend/controllers/accountController.js
@@ -2,60 +2,67 @@
 import ClientAccount from "../models/clientAccount.js";
 import EmployeeAccount from "../models/employeeAccount.js";
 import authController from "./authController.js";
-
 const displayInfo = async (req, res) => {
   try {
     //console.log("Grab logic for which account to grab and display info from.");
-    const clientResults = await ClientAccount.find({})
-    const employeeResults = await EmployeeAccount.find({})
-    res.status(200).send({body: {clients: clientResults, employees:employeeResults}});
+    const clientResults = await ClientAccount.find({});
+    const employeeResults = await EmployeeAccount.find({});
+    res
+      .status(200)
+      .send({ body: { clients: clientResults, employees: employeeResults } });
   } catch (e) {
     console.log(e.message);
     res.status(404).send("Unable to display account information.");
   }
 };
 
-const displayEmployees = async (req,res) => { // only display if admin
-    try{
-        const result = await EmployeeAccount.find({})
-        res.status(200).send({message: "Loading all available employees", result: result})
-    }
-    catch(e){
-        console.log(e.message)
-    }
-}
+const displayEmployees = async (req, res) => {
+  // only display if admin
+  try {
+    const result = await EmployeeAccount.find({});
+    res
+      .status(200)
+      .send({ message: "Loading all available employees", result: result });
+  } catch (e) {
+    console.log(e.message);
+  }
+};
 
-const displayClients = async (req,res) => {
-    try{
-        
-        const result = await ClientAccount.find({})
-        res.status(200).send({message: "Loading all available clients", result: result})
-    }
-    catch(e){
-        console.log(e.message)
-        res.status(404).send({message: "Unable to load clients. Try again.", body: result})
-    }
-}
+const displayClients = async (req, res) => {
+  try {
+    const result = await ClientAccount.find({});
+    res
+      .status(200)
+      .send({ message: "Loading all available clients", result: result });
+  } catch (e) {
+    console.log(e.message);
+    res
+      .status(404)
+      .send({ message: "Unable to load clients. Try again.", body: result });
+  }
+};
 
 const addClient = async (req, res) => {
-  const hashedPassword = await authController.genHash(req.body.password)
-  console.log(req.body)
-  console.log("In Add Client. Hash Pass:", hashedPassword)
+  console.log("Password Entered:", req.body.password);
+  const hashedPassword = await authController.genHash(req.body.password);
+  console.log("Request Body", req.body);
+  console.log("In Add Client. Hash Pass:", hashedPassword);
 
   try {
     console.log("Add Client");
-    const clientAcct = new ClientAccount({...req.body, password: hashedPassword.toString()})
-    console.log(clientAcct)
-    await clientAcct.save()
+    const clientAcct = new ClientAccount({
+      ...req.body,
+      password: hashedPassword,
+    });
+    console.log(clientAcct);
+    await clientAcct.save();
     res.status(200).json(clientAcct);
   } catch (e) {
     console.log(e);
-    res
-      .status(404)
-      .json({
-        message: "Unable to add new client. Review schema and try again.",
-        body_username: req.body.username,
-      });
+    res.status(404).json({
+      message: "Unable to add new client. Review schema and try again.",
+      body_username: req.body.username,
+    });
   }
 };
 
@@ -63,51 +70,56 @@ const addEmployee = async (req, res) => {
   try {
     console.log("Add Employee");
     //const employeeAcct = EmployeeAccount.create(req.body);
-    const employeeAcct = new EmployeeAccount(req.body)
-    employeeAcct = {...employeeAcct, password: encryption.genHash(req.body.password)}
-    await employeeAcct.save()
+    const employeeAcct = new EmployeeAccount(req.body);
+    employeeAcct = {
+      ...employeeAcct,
+      password: encryption.genHash(req.body.password),
+    };
+    await employeeAcct.save();
     res.status(200).json(employeeAcct);
-
   } catch (e) {
     console.log(e);
-    res
-      .status(404)
-      .json({
-        message: "Unable to add new employee. Review schema and try again.",
-        body: req.body,
-      });
+    res.status(404).json({
+      message: "Unable to add new employee. Review schema and try again.",
+      body: req.body,
+    });
   }
 };
 
-const deleteAccount =  async (req,res) => {
-    try{
-        console.log("Delete Account")
-    }
-    catch(e){
-        console.log(e)
-    }
-}
+const deleteAccount = async (req, res) => {
+  try {
+    console.log("Delete Account");
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-const clientSignIn = async(req,res) => {
-try{
-console.log(req)
-const connection = await fetch(BACKEND_URL+"/account/signin/client")
-const result = await connection.json()
-const findUserByName = result.find({"username":req.body?.username})
+const clientSignIn = async (req, res) => {
+  try {
+    console.log(req);
+    const connection = await fetch(BACKEND_URL + "/account/signin/client");
+    const result = await connection.json();
+    const findUserByName = result.find({ username: req.body?.username });
 
-const findUserByPhone = result.find({"phone":req.body?.contact?.phone}) // need logic to go thru array pf contact
+    const findUserByPhone = result.find({ phone: req.body?.contact?.phone }); // need logic to go thru array pf contact
 
-const findUserByEmail = result.find({"email":req.body?.contact?.email})
+    const findUserByEmail = result.find({ email: req.body?.contact?.email });
 
-const userFound = findUserByName || findUserByEmail || findUserByPhone
+    const userFound = findUserByName || findUserByEmail || findUserByPhone;
 
-console.log(userFound)
+    console.log(userFound);
 
-console.log(result)
-}
-catch(e){
-  console.log(e)
-  res.status(404).send({message: e.message})  // body: (username entered)
-}
-}
-export default { displayInfo, addEmployee, displayEmployees, displayClients, addClient, clientSignIn };
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+    res.status(404).send({ message: e.message }); // body: (username entered)
+  }
+};
+export default {
+  displayInfo,
+  addEmployee,
+  displayEmployees,
+  displayClients,
+  addClient,
+  clientSignIn,
+};
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,53 +1,62 @@
-// incomplete and untested, use for passwords! and usernames? 
+// incomplete and untested, use for passwords! and usernames?
 import bcrypt from "bcrypt";
 
 const saltRounds = 10;
 
+// async function genHash(attempted_password) {
+//   let salt;
+//   bcrypt.genSalt(saltRounds, (err, salt) => {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     // Salt generation successful, proceed to hash the password
+//     console.log("Salt generation succesful, proceed to hashing..");
 
-async function genHash(attempted_password){
-let salt;
- await bcrypt.genSalt(saltRounds, (err, salt) => {
-if (err) {
-    console.log(err)
-    return;
-}
-// Salt generation successful, proceed to hash the password
-console.log("Salt generation succesful, proceed to hashing..")
-console.log(salt)
+//     bcrypt.hash(attempted_password, salt, (err, hash) => {
+//       if (err) {
+//         console.log(err);
+//         return;
+//       }
+//       console.log("Hashing Succesful");
+//       console.log("Hashed password:", hash);
+//       return hash;
+//     });
+//   });
+// }
 
- bcrypt.hash(attempted_password, salt, (err, hash) => {
-    if (err) {
-        console.log(err)
-        return;
-    }
-console.log("Hashing Succesful")
-console.log('Hashed password:', hash);
-return hash
-});
-});
+const genHash = async (password) => {
+try{
+    const hash = await bcrypt.hash(password, saltRounds)
+    console.log('Hash Generated: ', hash)
+    return hash
 }
+catch(e){
+    console.log(e)
+    throw e
+}
+};
 
 // Password Comparison
 
- async function comparePassEntry(storedHashPass, userInputPass){
-  if(typeof storedHashPass == String && typeof userInputPass == String)
-   await bcrypt.compare(userInputPass, storedHashPass, (err, result) => {
-    if (err) {
-        console.error('Error comparing passwords:', err);
+function comparePassEntry(storedHashPass, userInputPass) {
+  if (typeof storedHashPass == String && typeof userInputPass == String)
+    bcrypt.compare(userInputPass, storedHashPass, (err, result) => {
+      if (err) {
+        console.error("Error comparing passwords:", err);
         return;
-    }
+      }
 
-if (result) {
-    // Passwords match, authentication successful
-    console.log('Passwords match! User authenticated.');
-    return true
-} else {
-    // Passwords don't match, authentication failed
-    console.log('Passwords do not match! Authentication failed.');
-    return false
+      if (result) {
+        // Passwords match, authentication successful
+        console.log("Passwords match! User authenticated.");
+        return true;
+      } else {
+        // Passwords don't match, authentication failed
+        console.log("Passwords do not match! Authentication failed.");
+        return false;
+      }
+    });
 }
-});
 
-}
-
-export default {genHash, comparePassEntry}
+export default { genHash, comparePassEntry };
