af57c0577974e2b18eb2579b331dc1a2fb6024a1
Nyala Jackson <nymjackson@gmail.com>
2025-11-20 11:28:14 -0500

    refactored encryption functions, moving to intergration with the db and logins.

--- backend/encryption.js
+++ backend/encryption.js
@@ -3,46 +3,47 @@ import bcrypt from "bcrypt";
 // https://www.freecodecamp.org/news/how-to-hash-passwords-with-bcrypt-in-nodejs/
 const saltRounds = 10;
 
-bcrypt.genSalt(saltRounds, (err, salt) => {
-  if (err) {
-    // Handle error
-    console.log(err);
+
+async function genHash(attempted_password){
+let salt;
+await bcrypt.genSalt(saltRounds, (err, salt) => {
+if (err) {
+    console.log(err)
     return;
-  }
-  console.log("Salt Successful. Proceeding...");
-  // Salt generation successful, proceed to hash the password
-});
+}
+// Salt generation successful, proceed to hash the password
+console.log("Salt generation succesful, proceed to hashing..")
+console.log(salt)
 
-//const userPassword = "user_password"; // Replace with the actual password
-const encryption = (userPassword) => bcrypt.hash(userPassword, salt, (err, hash) => {
-  if (err) {
-    // Handle error
-    return;
-  }
-
-  // Hashing successful, 'hash' contains the hashed password
-  console.log("Hashed password:", hash);
-});
-
-const decryption = bcrypt.compare(
-  userInputPassword,
-  storedHashedPassword,
-  (err, result) => {
+bcrypt.hash(attempted_password, salt, (err, hash) => {
     if (err) {
-      // Handle error
-      console.error("Error comparing passwords:", err);
-      return;
+        console.log(err)
+        return;
+    }
+console.log("Hashing Succesful")
+console.log('Hashed password:', hash);
+});
+});
+}
+
+// Password Comparison
+
+async function comparePassEntry(storedHashPass, userInputPass){
+  if(typeof storedHashPass == String && typeof userInputPass == String)
+  await bcrypt.compare(userInputPass, storedHashPass, (err, result) => {
+    if (err) {
+        console.error('Error comparing passwords:', err);
+        return;
     }
 
-    if (result) {
-      // Passwords match, authentication successful
-      console.log("Passwords match! User authenticated.");
-    } else {
-      // Passwords don't match, authentication failed
-      console.log("Passwords do not match! Authentication failed.");
-    }
-  }
-);
+if (result) {
+    // Passwords match, authentication successful
+    console.log('Passwords match! User authenticated.');
+} else {
+    // Passwords don't match, authentication failed
+    console.log('Passwords do not match! Authentication failed.');
+}
+});
 
-
+}
 
