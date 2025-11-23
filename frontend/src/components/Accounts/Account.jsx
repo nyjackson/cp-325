a677f7f49954142104087578fce886af7f4f96da
Nyala Jackson <nymjackson@gmail.com>
2025-11-23 15:09:24 -0500

    changed display all clients and employees to only for admins

--- frontend/src/components/Accounts/Account.jsx
+++ frontend/src/components/Accounts/Account.jsx
@@ -1,36 +1,117 @@
-import {useEffect} from 'react'
-import {useSelector, useDispatch} from 'react-redux'
-import {editProfile, linkAccount, delinkAccount, test, selectUser, selectLoginStatus} from './accountSlice'
-import Hero from '../Hero'
-function Account(){
-    const user = useSelector(selectUser)
-    const loginStatus = useSelector(selectLoginStatus)
-    const dispatch = useDispatch()
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  editProfile,
+  linkAccount,
+  delinkAccount,
+  test,
+  selectUser,
+  selectLoginStatus,
+} from "./accountSlice";
+import Hero from "../Hero";
+import { BACKEND_URL } from "../../App";
+
+function Account() {
+  const user = useSelector(selectUser);
+  const loginStatus = useSelector(selectLoginStatus);
+  const dispatch = useDispatch();
+  const [clientList, setClientList] = useState([])
+  const [empList, setEmpList] = useState([]); //temp
+  const [showList, setShowStatus] = useState(false)
+
+  useEffect( () => {
+    if(showList){
+        grabAllUsers()
+    }
+  }, [showList]);
+  console.log("In Account Component, getting user:", user);
+  
+  const joinDate = user.joined_on.split("T")[0];
+
+  async function grabAllUsers() {
+    try {
+      const response = await fetch(`${BACKEND_URL}/account`);
+      const data = await response.json();
+      console.log("Data",data)
+      setClientList([...data.body.clients])
+      setEmpList([...data.body.employees]);
+      console.log("data");
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  function showUserList() {
+    setShowStatus(!showList)
     
-    useEffect(()=> {
-        dispatch(test())
-    },[])
-
-console.log("In Account Component, getting user:", user)
-const joinDate = user.joined_on.split('T')[0]
-return(<>
-{loginStatus ? (
-    <div>
-        <Hero title = {`Welcome Back ${user.first_name}!`}/>
-        <h1>Account Details</h1>
-        <p> <b>Email: </b>{user.contact?.email || user.personal_contact?.email ||""}</p>
-        <p> <b>Phone: </b>{user.contact?.phone || user.personal_contact?.phone || ""}</p>
-        <p> <b>Joined On: </b>{joinDate}</p>
-        <p>{user.curr_appts?.length > 0 ? `Upcoming Appointments: ${user.curr_appts[0]}` : ''}</p>
-
-    </div>
-
-) : ''}
-{
-    user.access_role == 'Admin' ? console.log("Get All Employees and Clients") : ''
+  }
+  const listAllEmployees = empList.map((e) => <li key={e._id}>{e.username}</li>);
+  const listAllClients = clientList.map((c) => <li key={c._id}>{c.username}</li>);
+  return (
+    <>
+      {loginStatus ? (
+        <div>
+          <Hero title={`Welcome Back ${user.first_name}!`} />
+          <h1>Account Details</h1>
+          <p>
+            {" "}
+            <b>Email: </b>
+            {user.contact?.email || user.personal_contact?.email || ""}
+          </p>
+          <p>
+            {" "}
+            <b>Phone: </b>
+            {user.contact?.phone || user.personal_contact?.phone || ""}
+          </p>
+          <p>
+            {" "}
+            <b>Joined On: </b>
+            {joinDate}
+          </p>
+          <p>
+            {user.curr_appts?.length > 0
+              ? `Upcoming Appointments: ${user.curr_appts[0]}`
+              : ""}
+          </p>
+        </div>
+      ) : (
+        ""
+      )}
+      {user.access_role == "Admin"
+        ? (<>
+            <button onClick = {showUserList}> Display All Clients and Employees</button>
+        <div id = "user-lists">
+            {showList ? (
+            <div>
+            <h2>Employee List</h2>
+            <ul>{listAllEmployees}</ul>
+            </div>
+            ) : ''}
+        {showList ? (
+            <div>
+            <h2>Client List</h2>
+            <ul>{listAllClients}</ul>
+            </div>
+            ) : ''}
+        </div>
+        
+        </>
+    )
+        : ''}
+    </>
+  );
 }
 
-</>)
-}
+export default Account;
 
-export default Account
+
+//   const [empList, setEmpList] = useState([]) //temp
+
+//   async function displayAllInfo(){
+//       const response = await fetch(`${BACKEND_URL}/account`)
+//       const data = await response.json()
+//       setEmpList([...data.body.clients, ...data.body.employees])
+//       console.log("data")
+//   }
+
+//   console.log(empList)
+//   const listAll = empList.map((e) => <li key = {e._id}>{e.username}</li>)
--- frontend/src/components/Home/Homepage.jsx
+++ frontend/src/components/Home/Homepage.jsx
@@ -4,23 +4,12 @@ import {useEffect, useState} from 'react'
 import { BACKEND_URL } from '../../App';
 
 function Homepage() {
-  const [empList, setEmpList] = useState([]) //temp
 
-  async function displayAllInfo(){
-      const response = await fetch(`${BACKEND_URL}/account`)
-      const data = await response.json()
-      setEmpList([...data.body.clients, ...data.body.employees])
-      console.log("data")
-  }
-
-  console.log(empList)
-  const listAll = empList.map((e) => <li key = {e._id}>{e.username}</li>)
   return (
     <div id="home">
       <Hero title = {"PROMOTION"} content = {"Lorem Ipsum So You Don't Have To"}/>
       <h2>Wheels Up</h2>
-      <button onClick = {displayAllInfo}>Display All</button>
-      <ul>{listAll}</ul>
+      
     </div>
   );
 }
