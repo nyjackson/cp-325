6f14f437a6ac8008c5a3a4ba0db8b0323a51b559
Nyala Jackson <nymjackson@gmail.com>
2025-11-24 14:31:43 -0500

    Message box shows on signout

--- frontend/src/components/Accounts/SignOut.jsx
+++ frontend/src/components/Accounts/SignOut.jsx
@@ -1,18 +1,29 @@
 import SignIn from "./SignIn";
 import {useEffect} from 'react'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import { setUser, setLoginStatus } from "../slices/accountSlice";
+import { displayMessageStatus, setActive, signupSuccess } from "../slices/errorSlice";
+import MessageBox from "../MessageBox";
 
 function SignOut(){
     const dispatch = useDispatch()
+    const showMessage = useSelector(displayMessageStatus)
+    
 useEffect(() => {
+    dispatch(setActive(false))
     dispatch(setUser({}))
     dispatch(setLoginStatus(false))
     localStorage.removeItem("token")
+    handleDispatch()
 }, [])
+
+function handleDispatch(){
+    dispatch(signupSuccess("Successfully Signed Out."))
+    dispatch(setActive(true))
+}
 return (
     <>
-    <h2>Successfully Signed Out.</h2> {/**Message Box here */}
+    {showMessage ? <MessageBox /> : ''}
     <SignIn/>
     </>
 )
